b7169d30a03c3eb985d32d15ccfaf08f0d2d331f
Andrew Laptev <nickname.avast@gmail.com>
2022-06-13 22:52:56 +0300

    feat: add new entrypoint activity (user mode) Also add menu for switch to developer mode

--- app/src/main/java/com/example/ble_light/MainActivity.java
+++ app/src/main/java/com/example/ble_light/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.ble_light;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.ble_light.dev.MainActivityDev;
+import com.example.ble_light.dev.MultiConnectionActivityDev;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.activity_main_menu) {
+            final Intent intent = new Intent(this, MainActivityDev.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
